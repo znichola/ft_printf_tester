fbf7f9167a35f868950663d9f48a9aa7a269f1bc
znichola <znichola@student.42lausanne.ch>
2022-10-17 22:06:12 +0200

    bonus debugging

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 10:08:45 by znichola          #+#    #+#             */
-/*   Updated: 2022/10/17 19:41:50 by znichola         ###   ########.fr       */
+/*   Updated: 2022/10/17 22:04:37 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,24 @@ int	main(int ac, char **av)
 	
 	int test = 123; 
 
-	char *format = "int[%#8d] char[%c] str[%s] hex[% +#x] ptr[%p] uin[%u] NULL[%s] percent[%%]\n";
-	int _int = 123; int _hex = -123; int _uint = 123;	char _char = 'b'; char *_str = "test";
-	unsigned int _uin = 12345; 
-	// " %p %p ", LONG_MIN, LONG_MAX)
-	// printf("test us : <%- .2d>\n", test);
+	// char *format = "int[%d] char[%c] str[%s] hex[%x] ptr[%p] uin[%u] NULL[%s] percent[%%]\n";
+	// int _int = 123; int _hex = -123; int _uint = 123;	char _char = 'b'; char *_str = "test";
+	// unsigned int _uin = 12345; 
 	
-	// printf("binary test: %-s\n", ft_itoa_base(56, "01"));
-	// size_t len = 1;
-	size_t len = ft_printf(format, _int, _char, _str, _hex, LONG_MIN, _uin, NULL);
-	printf("\nreturn value is : %zu\n\n", len);
-	size_t len2 = printf(format, _int, _char, _str, _hex, LONG_MIN, _uin, NULL);
-	printf("\nreturn value is : %zu\n", len2);
+	// size_t len = ft_printf(format, _int, _char, _str, _hex, LONG_MIN, _uin, NULL);
+	// printf("\nreturn value is : %zu\n\n", len);
+	// size_t len2 = printf(format, _int, _char, _str, _hex, LONG_MIN, _uin, NULL);
+	// printf("\nreturn value is : %zu\n", len2);
+	
+#define	FRMT "{ %-2c }"
+#define	TST '0'
+	printf("\nmy  r eturn value is : %d\n\n", ft_printf(FRMT, TST));
+	printf("\nreal return value is : %d\n\n", printf(FRMT, TST));
+	// char *format = "test[%d]";
+	// size_t len = ft_printf(format, 1234);
+	// printf("\nreturn value is : %zu\n\n", len);
+	// size_t len2 = printf(format, 1234);
+	// printf("\nreturn value is : %zu\n", len2);
+
 	return (0);
 }
